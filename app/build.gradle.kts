dac69e8baee9caa53615bd24840b1f39a12eb81a
Nicos Nicolaou <nicosnicos16@gmail.com>
2025-09-05 00:19:55 +0300

    update README file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
 
     packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}" //for compose
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
 }
