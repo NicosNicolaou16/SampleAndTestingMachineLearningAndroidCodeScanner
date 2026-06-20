ed94502a4046bc9e34fec6e9a5c29f66c044369b
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-06-21 00:35:00 +0300

    update Lifecycle version to 2.11.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ composeCompiler {
 dependencies {
     implementation("androidx.core:core-ktx:1.19.0")
     implementation("androidx.fragment:fragment-ktx:1.8.9")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.11.0")
     implementation("androidx.activity:activity-compose:1.12.4")
     implementation(platform("androidx.compose:compose-bom:2026.02.00"))
     implementation("androidx.compose.ui:ui")
