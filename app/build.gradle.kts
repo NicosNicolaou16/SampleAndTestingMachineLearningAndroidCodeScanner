741555dc495f09c3a65f3ce99340b30a90d2261f
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-06-21 00:35:18 +0300

    update Activity Compose version to 1.13.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.19.0")
     implementation("androidx.fragment:fragment-ktx:1.8.9")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.11.0")
-    implementation("androidx.activity:activity-compose:1.12.4")
+    implementation("androidx.activity:activity-compose:1.13.0")
     implementation(platform("androidx.compose:compose-bom:2026.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
