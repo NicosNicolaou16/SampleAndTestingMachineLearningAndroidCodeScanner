65c811aeae193d79a81ec4ae6a519bc82803826a
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-06-21 00:36:14 +0300

    update Target SDK to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.sampleandtestingmachinelearningandroidcodescanner"
     buildToolsVersion = "36.0.0"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.sampleandtestingmachinelearningandroidcodescanner"
